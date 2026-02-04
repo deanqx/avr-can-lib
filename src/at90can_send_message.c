a100f4868ae97e9ed363ac8afaeec14ae2025a38
deanqx <110404616+deanqx@users.noreply.github.com>
2026-02-04 14:30:48 +0100

    feat: reduce memory usage by removing unnecessary pointer

--- src/at90can_send_message.c
+++ src/at90can_send_message.c
@@ -50,14 +50,10 @@ void at90can_copy_message_to_mob(const can_t *msg)
 		// extended CAN ID
 		CANCDMOB |= (1 << IDE);
 		
-		CANIDT4 = (uint8_t)  msg->id << 3;
-		
-		uint32_t temp = msg->id << 3;
-		uint8_t *ptr = (uint8_t *) &temp;
-		
-		CANIDT3 = *(ptr + 1);
-		CANIDT2 = *(ptr + 2);
-		CANIDT1 = *(ptr + 3);
+		CANIDT4 = (uint8_t) (msg->id << 3);
+		CANIDT3 = (uint8_t) (msg->id >> 5);
+		CANIDT2 = (uint8_t) (msg->id >> (5 + 8));
+		CANIDT1 = (uint8_t) (msg->id >> (5 + 8 + 8));
 	}
 	else {
 		// standard CAN ID
